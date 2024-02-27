0554fe7ea448fc0d6e6969f48c10debfc6f8ce8f
Luis Salazar <lsalab@gmail.com>
2024-02-26 18:27:33 -0800

    rm deprecated submodule

--- nefics/protos/IEC61850/iec61850_mms/_internal.py
+++
@@ -1,9 +0,0 @@
-from scapy.packet import Packet, Raw
-from .mms import MMS
-from .iso8327_1.packets import ISO_8327_1_Session
-
-
-class Pseudo_Layer(Packet):
-    def guess_payload_class(self, payload):
-        res = ISO_8327_1_Session().guess_payload_class(payload)
-        return res if res != Raw else MMS
