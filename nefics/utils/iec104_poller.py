bdc7c2174ee55a69b05e7c0b3d65afdd3c06b1f7
Luis Salazar <lsalab@gmail.com>
2024-02-27 18:04:53 -0800

    refactor poller

--- nefics/utils/iec104_poller.py
+++ nefics/utils/iec104_poller.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+from threading import Thread
 from netaddr import valid_ipv4
-from types import FrameType
 from socket import AF_INET, IPPROTO_TCP, SOCK_STREAM, socket, timeout
+from time import sleep
+from types import FrameType
+from typing import Optional
 
 # NEFICS imports
-from nefics.IEC104.const import DPI_ENUM
-from nefics.IEC104.dissector import APDU, APCI
+from nefics.protos.iec10x.enums import DPI_ENUM
+from nefics.protos.iec10x.packets import APDU, APCI
+from nefics.protos.iec10x.iec104 import IEC104_PORT, TIMEOUT_T1, TIMEOUT_T2
 
-IEC104_PORT = 2404
-IEC104_T1 = 15
-BUFFER_SIZE = 65536
-
-IOA_ADDR_MAP = {
-    1001: 'Voltage',
-    1002: 'Current'
-}
+BUFFER_SIZE = 260
 
 class IEC104Poller(object):
 
@@ -25,7 +22,7 @@ class IEC104Poller(object):
         assert valid_ipv4(address)
         self._terminate = False
         self._sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
-        self._sock.settimeout(IEC104_T1)
+        self._sock.settimeout(TIMEOUT_T1)
         try:
             self._sock.connect((address, IEC104_PORT))
         except timeout:
@@ -33,44 +30,38 @@ class IEC104Poller(object):
             sys.exit()
         print('[+] Connection established')
     
-    def terminate(self, signum:int, stack_frame:FrameType):
+    def terminate(self, signum : int, stack_frame : Optional[FrameType]):
         self._terminate = True
+
+    def _keep_alive(self):
+        while not self._terminate:
+            apdu : APDU = APDU()/APCI(type=0x03, UType=0x10)
+            self._sock.send(apdu.build())
+            sleep(TIMEOUT_T2)
 
     def loop(self):
         try:
+            ka_thread = Thread(target=self._keep_alive)
             print('[*] Sending STARTDT U-Frame ... ', end='')
-            apdu = APDU()/APCI(ApduLen=4, Type=0x03, UType=0x01)
+            apdu = APDU()/APCI(type=0x03, UType=0x01)
             self._sock.send(apdu.build())
             data = self._sock.recv(BUFFER_SIZE)
             apdu = APDU(data)
-            if apdu['APCI'].Type != 0x03 or apdu['APCI'].UType != 0x02:
+            if apdu['APCI'].type != 0x03 or apdu['APCI'].UType != 0x02:
                 print(f'ERROR\r\n[!] Unexpected Frame: {repr(apdu)}')
             print('Confirmed')
+            ka_thread.start()
             while not self._terminate:
                 data = self._sock.recv(BUFFER_SIZE)
                 apdu = APDU(data)
-                if apdu['ASDU'].TypeId == 36:
-                    print(f"[+] Received type 36 ASDU :: [{IOA_ADDR_MAP[apdu['IOA36'].IOA]}] Value: {apdu['IOA36'].Value}")
-                elif apdu['ASDU'].TypeId == 3:
-                    print(f"[+] Received type 3 ASDU :: [Breaker status] Breaker ID: {apdu['IOA3'].IOA} Status: {DPI_ENUM[apdu['DIQ'].DPI]} (0x{apdu['DIQ'].DPI:02x})")
-                else:
-                    print(f'[!] Received an unknown ASDU :: {repr(apdu)}')
-                print('[*] Sending TESTFR U-Frame ... ', end='')
-                apdu = APDU()/APCI(ApduLen=4, Type=0x03, UType=0x10)
-                self._sock.send(apdu.build())
-                data = self._sock.recv(BUFFER_SIZE)
-                apdu = APDU(data)
-                if apdu['APCI'].Type != 0x03 or apdu['APCI'].UType != 0x20:
-                    print(f'FATAL\r\n[!] Unexpected frame: {repr(apdu)}')
-                    self._sock.close()
-                    sys.exit()
-                print('Confirmed')
+                print(f'[!] Received APDU :: {repr(apdu)}')
+            ka_thread.join()
             print('[*] Sending STOPDT U-Frame ... ')
-            apdu = APDU()/APCI(ApduLen=4, Type=0x03, UType=0x04)
+            apdu = APDU()/APCI(type=0x03, UType=0x04)
             self._sock.send(apdu.build())
             data = self._sock.recv(BUFFER_SIZE)
             apdu = APDU(data)
-            while apdu['APCI'].Type != 0x03 or apdu['APCI'].UType != 0x08:
+            while apdu['APCI'].type != 0x03 or apdu['APCI'].UType != 0x08:
                 print('[!] Received pending Frame:', repr(apdu))
                 data = self._sock.recv(BUFFER_SIZE)
                 apdu = APDU(data)
